66965dbcf8265358accf1cce78ce680417c6390b
eli00i1233 <eli00i1233@gmail.com>
2023-07-03 10:01:27 +0400

    ResumeDbAppUsingJDBC

---
+++ src/main/java/com/company/dao/inter/AbstractDao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.dao.inter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Lenovo
+ */
+public abstract class AbstractDao {
+    public static Connection connect() throws Exception, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/resume";
+        String username = "root";
+        String password = "12345";
+        Connection c = DriverManager.getConnection(url, username, password);
+        return c;
+    }
+}
